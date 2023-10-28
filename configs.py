dd09c1d3dc8e7060c1613c229814e797d69a6545
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2023-10-28 12:56:26 +0545

    Config Added ✅

---
+++ configs.py
@@ -0,0 +1,15 @@
+from os import path, getenv
+import os
+
+class Config:
+    API_ID = int(getenv("API_ID", "0112234"))
+    API_HASH = getenv("API_HASH", "abcdefg")
+    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:rkndeveloperDEhdhyjjvjjftSEW")
+    UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "RknDeveloper")
+    UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1001819787652"))
+    ADMIN = list(map(int, getenv("ADMIN").split()))
+    MONGO_URI = getenv("MONGO_URI", "")
+    RKN_PIC = os.environ.get("RKN_PIC", "https://graph.org/file/e846f9375e9d4f4975ce4.jpg")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME","")
+    
+rkn = Config()
