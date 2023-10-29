1848fe1aefa8c5a5474d8bbe69782ff2955a88e4
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2023-10-29 06:55:16 +0545

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,6 +9,7 @@ class Config:
     UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1001819787652"))
     ADMIN = list(map(int, getenv("ADMIN").split()))
     MONGO_URI = getenv("MONGO_URI", "")
+    PORT = os.environ.get("PORT", "8080")
     RKN_PIC = os.environ.get("RKN_PIC", "https://graph.org/file/e846f9375e9d4f4975ce4.jpg")
     BOT_USERNAME = os.environ.get("BOT_USERNAME","")
     
