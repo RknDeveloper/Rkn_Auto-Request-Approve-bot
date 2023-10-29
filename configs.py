c400a8b8c88da45187486d57f97257b38bf96083
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2023-10-29 08:55:31 +0545

    LOG_CHANNEL Verriable 🥀 Added ✅

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config:
     UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1001819787652"))
     ADMIN = list(map(int, getenv("ADMIN").split()))
     MONGO_URI = getenv("MONGO_URI", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001944995812"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002120760645"))
     
     PORT = os.environ.get("PORT", "8080")
     RKN_PIC = os.environ.get("RKN_PIC", "https://graph.org/file/e846f9375e9d4f4975ce4.jpg")
