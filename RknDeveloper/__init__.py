6afcc28c47e51524012a7d66468c6e8f9b586da0
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-11-14 19:21:43 +0545

    Updated v1.0.0 version

---
+++ RknDeveloper/__init__.py
@@ -0,0 +1,40 @@
+#  Telegram MTProto API Client Library for Pyrogram
+#  Copyright (C) 2017-present RknDeveloper <https://github.com/RknDeveloper>
+#  I am a telegram bot, I created it using pyrogram library. https://github.com/pyrogram
+"""
+Apache License 2.0
+Copyright (c) 2022  @RknDeveloper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Telegram Link : https://t.me/RknDeveloper
+Repo Link : https://github.com/RknDeveloper/Rkn_Auto-Request-Approve-bot
+License Link : https://github.com/RknDeveloper/Rkn_Auto-Request-Approve-bot/blob/main/LICENSE
+"""
+
+__name__ = "Auto-Request-Approve-Bot"
+__version__ = "1.0.0"
+__license__ = " Apache License, Version 2.0"
+__copyright__ = "Copyright (C) 2022-present Rkn Developer <https://github.com/RknDeveloper>"
+__programer__ = "<a href=https://github.com/RknDeveloper/Rkn_Auto-Request-Approve-bot>RknDeveloper</a>"
+__library__ = "<a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>"
+__language__ = "<a href=https://www.python.org/>Pyᴛʜᴏɴ 3</a>"
+__database__ = "<a href=https://cloud.mongodb.com/>Mᴏɴɢᴏ DB</a>"
+__developer__ = "<a href=https://t.me/Digital_Botz>Digital Botz</a>"
+__maindeveloper__ = "<a href=https://t.me/RknDeveloper>RknDeveloper</a>"
+
+# main copyright herders (©️)
+# I have been working on this repo since 2022
