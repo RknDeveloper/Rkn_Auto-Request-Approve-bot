70cf9d5062f2145d097455ed211b79dca18c0287
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-11-14 19:25:43 +0545

    Updated v2.0.0 version lastest @RknDeveloper

--- bot.py
+++ bot.py
@@ -1,27 +1,59 @@
-#(©) @RknDeveloper https://github.com/RknDeveloper/Rkn_Auto-Request-Approve-bot
-# Please Don't remove Credit 😢
-import time, os
-import logging
-import logging.config
+# (c) @RknDeveloperr
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Botz
+# Developer @RknDeveloperr
+# Special Thanks To (https://github.com/JayMahakal98)
+# Update Channel @Digital_Botz & @DigitalBotz_Support
+
+"""
+Apache License 2.0
+Copyright (c) 2022 @RknDeveloper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Telegram Link : https://t.me/RknDeveloper
+Repo Link : https://github.com/RknDeveloper/Rkn_Auto-Request-Approve-bot
+License Link : https://github.com/RknDeveloper/Rkn_Auto-Request-Approve-bot/blob/main/LICENSE
+"""
+
+# extra imports
+import time, os, logging, logging.config
 from aiohttp import web
 from datetime import datetime
 from pytz import timezone
-from pyrogram.raw.all import layer
-from configs import rkn1
-from RknDeveloper.untils.add import set_commands
-from pyrogram import Client, __version__
-from RknDeveloper.web_support import web_server
 
+# pyrogram imports
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+
+# bots imports
+from RknDeveloper.web_support import web_server
+from configs import rkn1
+
+# logging print 
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
  
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
-            "approver",
+            "auto-approver",
             api_id=rkn1.API_ID,
             api_hash=rkn1.API_HASH,
             bot_token=rkn1.BOT_TOKEN,
@@ -31,19 +63,23 @@ class Bot(Client):
         await super().start()
         me = await self.get_me()
         self.mention = me.mention
-        self.username = me.username 
+        self.username = me.username
+        self.uptime = rkn1.BOT_UPTIME
+        
         if rkn1.WEBHOOK:
             app = web.AppRunner(await web_server())
             await app.setup()
             bind_address = "0.0.0.0"
             await web.TCPSite(app, bind_address, rkn1.PORT).start()
-        logging.info(f"{me.first_name} ✅✅ BOT started successfully ✅✅")
+            
+        logging.info(f"{me.first_name} Restarted.....")
         logging.info(rkn1.LOGO)
-        await set_commands(self)
+        
         for id in rkn1.ADMIN:
             try:
-                await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")
+                await self.send_message(id, f"**__{me.first_name}  Restarted......__**")
             except: pass
+            
         if rkn1.LOG_CHANNEL:
             try:
                 curr = datetime.now(timezone("Asia/Kolkata"))
@@ -52,9 +88,17 @@ class Bot(Client):
                 await self.send_message(rkn1.LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")                                
             except:
                 print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
+                
     async def stop(self, *args):
       await super().stop()      
       logging.info("Bot Stopped 🙄")
         
 bot = Bot()
 bot.run()
+
+
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Botz
+# Developer @RknDeveloperr
+# Update Channel @Digital_Botz & @DigitalBotz_Support
--- configs.py
+++ configs.py
@@ -1,16 +1,50 @@
+# (c) @RknDeveloperr
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Botz
+# Developer @RknDeveloperr
+# Special Thanks To (https://github.com/JayMahakal98)
+# Update Channel @Digital_Botz & @DigitalBotz_Support
+
+"""
+Apache License 2.0
+Copyright (c) 2022 @RknDeveloper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Telegram Link : https://t.me/RknDeveloper
+Repo Link : https://github.com/RknDeveloper/Rkn_Auto-Request-Approve-bot
+License Link : https://github.com/RknDeveloper/Rkn_Auto-Request-Approve-bot/blob/main/LICENSE
+"""
+
 from os import path, getenv
 import os, time 
-#from pyrogram.types import BotCommand
 
 class Config:
     API_ID = int(getenv("API_ID", "0112234"))
     API_HASH = getenv("API_HASH", "abcdefg")
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:rkndeveloperDEhdhyjjvjjftSEW")
-    UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "RknDeveloper")
-    UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1001819787652"))
+ 
+    FORCE_SUB = int(getenv("FORCE_SUB", "-1001819787652"))
     ADMIN = list(map(int, getenv("ADMIN", "6151758586").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002120760645"))
+    
+    # database configs
+    DB_URL = os.environ.get("DB_URL", "")
+    DB_NAME = os.environ.get("DB_NAME", "")
     
     #web response 
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
@@ -18,7 +52,7 @@ class Config:
     PORT = os.environ.get("PORT", "8080")
     
     RKN_PIC = os.environ.get("RKN_PIC", "https://graph.org/file/e846f9375e9d4f4975ce4.jpg")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME","Rkn_AutoRequestApprovebot")
+    
  # Subsprice Gif & Auto Request Accept
     SURPRICE = os.environ.get("SURPRICE", "https://telegra.ph/file/a5a2bb456bf3eecdbbb99.mp4 https://telegra.ph/file/03c6e49bea9ce6c908b87.mp4 https://telegra.ph/file/9ebf412f09cd7d2ceaaef.mp4 https://telegra.ph/file/293cc10710e57530404f8.mp4 https://telegra.ph/file/506898de518534ff68ba0.mp4 https://telegra.ph/file/dae0156e5f48573f016da.mp4 https://telegra.ph/file/3e2871e714f435d173b9e.mp4 https://telegra.ph/file/714982b9fedfa3b4d8d2b.mp4 https://telegra.ph/file/876edfcec678b64eac480.mp4 https://telegra.ph/file/6b1ab5aec5fa81cf40005.mp4 https://telegra.ph/file/b4834b434888de522fa49.mp4").split()
 
@@ -30,3 +64,10 @@ class Config:
 ──────────  ──────────────────────────────"""
 
 rkn1 = Config()
+
+
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Botz
+# Developer @RknDeveloperr
+# Update Channel @Digital_Botz & @DigitalBotz_Support
